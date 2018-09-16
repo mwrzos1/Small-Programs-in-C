e438efa584020e069fbb19487e3ab79be7b6034a
Marcin <mwrzos1@students.towson.edu>
2018-09-16 00:51:34 -0400

    quadratic formula roots

--- roots.c
+++ roots.c
@@ -3,25 +3,27 @@
 #include <stdlib.h>
 int main(){
 	
-	      double a,b,c,x1, x2 ,x3,formula;
+		  double a,b,c,x1, x2 ,x3,formula;
 	      printf("Please input three numbers a,b,c \n");
 	      scanf("%lf %lf %lf", &a, &b, &c);
 
 	      formula = b*b- 4*a*c;
 	      if(a==0){
-	       printf("Can't do it! A has to be other than 0 \n");
-	       }
+	               printf("Can't do it! A has to be other than 0 \n");
+	      }
           else  if ((b*b)-(4*a*c) > 0){
-	 	  x1 = (-b+sqrt(formula))/ (2*a);
-		  x2 = (-b-sqrt(formula))/ (2*a);
-		  printf("X1 = %lf and X2 = %lf \n", x1, x2);
+			  
+	 	           x1 = (-b+sqrt(formula))/ (2*a);
+		           x2 = (-b-sqrt(formula))/ (2*a);
+		           printf("X1 = %lf and X2 = %lf \n", x1, x2);
 	      }
 	      else if (formula ==0){
-		  x3=((-b)/(2*a));
-		  printf("X = %lf \n", x3);
+			  
+		           x3=((-b)/(2*a));
+		           printf("X = %lf \n", x3);
 	      }
 	      else{
-		  printf("Imaginary numbers! \n");
+		           printf("Imaginary numbers! \n");
 	      }
 	      return 0;
         }
